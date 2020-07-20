ccb57bfd5f17917a98afe7628d31e0a31358ad76
Amir <AmirMV2006@gmail.com>
2020-07-20 21:36:55 +0200

    recurring sample data

--- sample-application/src/main/java/ir/amv/os/sample/application/SampleApplication.java
+++ sample-application/src/main/java/ir/amv/os/sample/application/SampleApplication.java
@@ -3,6 +3,9 @@ package ir.amv.os.sample.application;
 import ir.amv.os.protocol.free.integration.api.EnableProtocolFreeApi;
 import ir.amv.os.protocol.free.integration.api.order.Order;
 import ir.amv.os.protocol.free.integration.api.order.OrderGateway;
+import java.security.SecureRandom;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +16,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SampleApplication {
 
   public static void main(String[] args) {
-    ConfigurableApplicationContext applicationContext = SpringApplication.run(SampleApplication.class, args);
+    ConfigurableApplicationContext applicationContext = SpringApplication
+        .run(SampleApplication.class, args);
     OrderGateway orderGateway = applicationContext.getBean(OrderGateway.class);
-    orderGateway.submitOrder(Order.builder()
-        .orderId(UUID.randomUUID())
-        .username("amir")
-        .cost(100)
-        .build());
+    SecureRandom random = new SecureRandom();
+    new Timer()
+        .scheduleAtFixedRate(new TimerTask() {
+          @Override
+          public void run() {
+            orderGateway.submitOrder(Order.builder()
+                .orderId(UUID.randomUUID())
+                .username("amir")
+                .cost(random.nextInt(1000))
+                .build());
+          }
+        }, 100, 1000);
   }
 }
