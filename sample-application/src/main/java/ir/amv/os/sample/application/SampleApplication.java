d92b7501477333933ec7ca6d38083cec8a5544bf
Amir <AmirMV2006@gmail.com>
2020-07-20 21:34:28 +0200

    working sample application

--- sample-application/src/main/java/ir/amv/os/sample/application/SampleApplication.java
+++ sample-application/src/main/java/ir/amv/os/sample/application/SampleApplication.java
@@ -4,30 +4,21 @@ import ir.amv.os.protocol.free.integration.api.EnableProtocolFreeApi;
 import ir.amv.os.protocol.free.integration.api.order.Order;
 import ir.amv.os.protocol.free.integration.api.order.OrderGateway;
 import java.util.UUID;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@Configuration
+@SpringBootApplication
 @EnableProtocolFreeApi
 public class SampleApplication {
 
-  @Bean
-  public InitializingBean afterStartup(
-      OrderGateway orderGateway
-  ) {
-    return () -> {
-      Order order = Order.builder()
-          .orderId(UUID.randomUUID())
-          .username("amir")
-          .cost(10)
-          .build();
-      orderGateway.submitOrder(order);
-    };
-  }
-
   public static void main(String[] args) {
-    SpringApplication.run(SampleApplication.class, args);
+    ConfigurableApplicationContext applicationContext = SpringApplication.run(SampleApplication.class, args);
+    OrderGateway orderGateway = applicationContext.getBean(OrderGateway.class);
+    orderGateway.submitOrder(Order.builder()
+        .orderId(UUID.randomUUID())
+        .username("amir")
+        .cost(100)
+        .build());
   }
 }
